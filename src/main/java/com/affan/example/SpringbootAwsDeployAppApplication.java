41a5aaeac8af95664914d66736b2c10f2ff993d6
Md Anish <anish15java@gmail.com>
2023-10-02 18:56:02 +0530

    First-Aws

---
+++ src/main/java/com/affan/example/SpringbootAwsDeployAppApplication.java
@@ -0,0 +1,13 @@
+package com.affan.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsDeployAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsDeployAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/affan/example/SpringbootAwsDeployAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.affan.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsDeployAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
